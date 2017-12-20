2b44a6e72559c427b19f10596e92f6ac101ff751
Luka <luka.babic@fivemintes.eu>
2017-12-20 13:40:55 +0100

    Showing toast from MainActivity.

--- app/src/main/java/com/android/template/MainActivity.kt
+++ app/src/main/java/com/android/template/MainActivity.kt
@@ -2,11 +2,14 @@ package com.android.template
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Hello world toast", Toast.LENGTH_SHORT).show()
     }
 }
