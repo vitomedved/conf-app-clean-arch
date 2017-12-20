2f74fee87a37ff44e4ca87cbefc2e9b66333d981
Luka <luka.babic@fivemintes.eu>
2017-12-20 13:55:25 +0100

    Fixing module dependency issue.

--- app/src/main/java/com/android/template/MainActivity.kt
+++ app/src/main/java/com/android/template/MainActivity.kt
@@ -3,6 +3,9 @@ package com.android.template
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import template.android.com.data.DataKotlinInvoker
+import template.android.com.device.DeviceKotlinInvoker
+import template.android.com.domain.DomainKotlinInvoker
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +14,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Toast.makeText(this, "Hello world toast", Toast.LENGTH_SHORT).show()
+
+        // To ensure all modules are well connected.
+        DataKotlinInvoker().invoke()
+        DeviceKotlinInvoker().invoke()
+        DomainKotlinInvoker().invoke()
     }
 }
