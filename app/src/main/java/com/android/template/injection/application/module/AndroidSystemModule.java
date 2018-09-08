d62f8710c245e32b32b222414b43d98289ab8680
lbabic <luka.babic@asseco-see.hr>
2018-09-08 11:40:21 +0200

    Adding new items to AndroidSystemModule.

--- app/src/main/java/com/android/template/injection/application/module/AndroidSystemModule.java
+++ app/src/main/java/com/android/template/injection/application/module/AndroidSystemModule.java
@@ -1,8 +1,15 @@
 package com.android.template.injection.application.module;
 
+import android.annotation.SuppressLint;
 import android.app.NotificationManager;
+import android.bluetooth.BluetoothManager;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
+import android.net.wifi.WifiManager;
+import android.telephony.TelephonyManager;
 
 import com.android.template.injection.ForApplication;
 
@@ -16,20 +23,67 @@ public final class AndroidSystemModule {
 
     @Provides
     @Singleton
+    BluetoothManager provideBluetoothManager(@ForApplication final Context context) {
+        return BluetoothManager.class.cast(context.getSystemService(Context.BLUETOOTH_SERVICE));
+    }
+
+    @Provides
+    @Singleton
     ConnectivityManager provideConnectivityManager(@ForApplication final Context context) {
-        return (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return ConnectivityManager.class.cast(context.getSystemService(Context.CONNECTIVITY_SERVICE));
+    }
+
+    @Provides
+    @Singleton
+    ContentResolver provideContentResolver(@ForApplication final Context context) {
+        return context.getContentResolver();
+    }
+
+    @Provides
+    @Singleton
+    LocationManager provideLocationManager(@ForApplication final Context context) {
+        return LocationManager.class.cast(context.getSystemService(Context.LOCATION_SERVICE));
     }
 
     @Provides
     @Singleton
     NotificationManager provideNotificationManager(@ForApplication final Context context) {
-        return (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        return NotificationManager.class.cast(context.getSystemService(Context.NOTIFICATION_SERVICE));
+    }
+
+    @Provides
+    @Singleton
+    PackageManager providePackageManager(@ForApplication final Context context) {
+        return context.getPackageManager();
+    }
+
+    @Provides
+    @Singleton
+    TelephonyManager provideTelephonyManager(@ForApplication final Context context) {
+        return TelephonyManager.class.cast(context.getSystemService(Context.TELEPHONY_SERVICE));
+    }
+
+    @SuppressLint("WifiManagerPotentialLeak")
+    @Provides
+    @Singleton
+    WifiManager provideWifiManager(@ForApplication final Context context) {
+        return WifiManager.class.cast(context.getSystemService(Context.WIFI_SERVICE));
     }
 
     public interface Exposes {
 
+        BluetoothManager bluetoothManager();
+
         ConnectivityManager connectivityManager();
 
+        LocationManager locationManager();
+
         NotificationManager notificationManager();
+
+        PackageManager packageManager();
+
+        TelephonyManager telephonyManager();
+
+        WifiManager wifiManager();
     }
 }
