6e621942d02f5fbc8318261a87bf39722fbe4942
lbabic <luka.babic@asseco-see.hr>
2018-09-08 12:01:00 +0200

    Adding equals and hashcode to networkdata.

--- device/src/main/java/template/android/com/device/connectivity/NetworkData.java
+++ device/src/main/java/template/android/com/device/connectivity/NetworkData.java
@@ -9,4 +9,36 @@ public final class NetworkData {
         this.hasInternetConnection = hasInternetConnection;
         this.isMobileConnection = isMobileConnection;
     }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        final NetworkData that = (NetworkData) o;
+
+        if (hasInternetConnection != that.hasInternetConnection) {
+            return false;
+        }
+        return isMobileConnection == that.isMobileConnection;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (hasInternetConnection ? 1 : 0);
+        result = 31 * result + (isMobileConnection ? 1 : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "NetworkData{" +
+                "hasInternetConnection=" + hasInternetConnection +
+                ", isMobileConnection=" + isMobileConnection +
+                '}';
+    }
 }
